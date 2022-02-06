37273e009e7b2029f795f276eece4614668f1977
Khaair <khair.cse.iu@gmail.com>
2022-02-06 18:34:18 +0600

    Weather  app

--- src/App.js
+++ src/App.js
@@ -1,46 +1,75 @@
-import React from "react";
+import Axios from "axios";
+import React, { useState } from "react";
 import "./App.css";
-import Weather from "./Component/Weather";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weathers, setWeathers] = useState([]);
   const API_KEY = "36322319baf21d82da4b8b8bdf9388b1";
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`;
+
+  async function getWeather() {
+    let result = await Axios.get(url);
+    setWeathers(result.data);
+    console.log(result.data);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getWeather();
+  };
   return (
     <>
-    <div className="Containers">
-      <div className="card">
-        <img
-          className="weather01"
-          src="./img/weather.jpg"
-          alt="Avatar"
-          style={{ width: "100%" }}
-        />
-        <div className="container">
-          <h4>
-            <b>React Weather Application</b>
-          </h4>
-          <h3>Find Weather of your city</h3>
+      <div className="Containers">
+        <div className="card">
+          <img
+            className="weather01"
+            src="./img/weather.jpg"
+            alt="Avatar"
+            style={{ width: "100%" }}
+          />
+          <div className="container">
+            <h2 className="kk">
+              React Weather Application
+            </h2>
+            <h3 className="kk">Find Weather Of Your City</h3>
 
-          <div className="wrap">
-            <div className="search">
-              <input
-                type="text"
-                className="searchTerm"
-                placeholder="Enter Your City Name"
-              />
-              <button type="submit" className="searchButton">
-                <i className="fa fa-search" />
-                Search
-              </button>
+            <div className="wrap">
+              <div className="search">
+                <form onSubmit={onSubmit}>
+                  <input
+                    type="text"
+                    className="searchTerm"
+                    placeholder="Enter Your City Name"
+                    onChange={(e) => setQuery(e.target.value)}
+                    value={query}
+                  />
+                  <button
+                    onClick={getWeather}
+                    type="submit"
+                    className="searchButton"
+                  >
+                    <i className="fa fa-search" />
+                    Search
+                  </button>
+                </form>
+                <div>
+                  {/* {JSON.stringify(weathers)} */}
+              
+                </div>
+              </div>
             </div>
           </div>
+          <h3 className="kk">Temperature: {weathers.main.temp}</h3>
+          <h3 className="kk">Country Name: {weathers.sys.country}</h3>
+          <h3 className="kk">Wind Speed: {weathers.wind.speed} Km/hour</h3>
+          <p></p>
         </div>
+     
       </div>
-  
-    </div>
-        <Weather/>
-        </>
+      {/* <Weather /> */}
+    </>
   );
 }
 
