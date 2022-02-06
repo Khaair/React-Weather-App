b95c9ee57455aed5aea9f6835c5bbb003419a04a
Khaair <khair.cse.iu@gmail.com>
2022-02-06 11:36:43 +0600

    Recipi app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Weather from "./Component/Weather";
 
 function App() {
+  const API_KEY = "36322319baf21d82da4b8b8bdf9388b1";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid=${API_KEY}`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="Containers">
+      <div className="card">
+        <img
+          className="weather01"
+          src="./img/weather.jpg"
+          alt="Avatar"
+          style={{ width: "100%" }}
+        />
+        <div className="container">
+          <h4>
+            <b>React Weather Application</b>
+          </h4>
+          <h3>Find Weather of your city</h3>
+
+          <div className="wrap">
+            <div className="search">
+              <input
+                type="text"
+                className="searchTerm"
+                placeholder="Enter Your City Name"
+              />
+              <button type="submit" className="searchButton">
+                <i className="fa fa-search" />
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+  
     </div>
+        <Weather/>
+        </>
   );
 }
 
---
+++ src/Component/Weather.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../App.css";
+
+function Weather() {
+  return (
+    
+      <div className="Containers">
+        <div className="card">
+          <img
+            className="weather01"
+            src="./img/weather.jpg"
+            alt="Avatar"
+            style={{ width: "100%" }}
+          />
+          <h4>
+            <b>React Weather Application</b>
+          </h4>
+        </div>
+      </div>
+    
+  );
+}
+
+export default Weather;
